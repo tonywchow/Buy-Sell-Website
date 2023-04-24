44155a4d528dd687c0cd9a443fbe45085e727d6f
Tony Chow <tchow9@gmail.com>
2023-04-24 05:44:02 -0700

    Add getUserFavourites and getAllProducts

--- routes/database.js
+++ routes/database.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -11,6 +12,23 @@ const pool = new Pool({
 pool.connect().then(() => {
   console.log("Connected to database!");
 });
+
+const getAllProducts = (limit) => {
+  limit = 10;
+
+  let queryString = `
+  SELECT * FROM products
+  ORDER BY date ASC
+  `;
+
+  return pool
+    .query(queryString)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((error) => console.log(error));
+};
 
 /*
 Adding product to database
@@ -31,13 +49,37 @@ const addProduct = function (products) {
   ];
   return pool
     .query(queryString, values)
-    .then((res) => {
-      console.log(res.rows);
-      return res.rows;
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((error) => console.log(error));
+};
+
+//Getting user favourites
+const getUserFavourites = function (admin_id) {
+  const queryString = `
+  SELECT product.*, favourites.* FROM favourites
+  JOIN products on products.id = favourites.product_id
+  JOIN users on users.id = favourites.user_id
+  WHERE user_id = $1
+  GROUP BY favourites.id
+  ORDER BY date ASC
+  `;
+
+  const values = [guest_id];
+
+  return client
+    .query(queryString, values)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
     })
     .catch((error) => console.log(error));
 };
 
 module.exports = {
   addProduct,
+  getUserFavourites,
+  getAllProducts,
 };
