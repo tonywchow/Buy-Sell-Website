da81c9ed50e4d07a3aceb8362a8cddf356334111
Tony Chow <tchow9@gmail.com>
2023-04-26 20:22:21 -0700

    create post ejs function

--- server.js
+++ server.js
@@ -1,72 +1,62 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  '/styles',
+  "/styles",
   sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
     isSass: false, // false => scss, true => sass
   })
 );
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
+const userApiRoutes = require("./routes/users-api");
+const widgetApiRoutes = require("./routes/widgets-api");
+const usersRoutes = require("./routes/users");
+const createPost = require("./routes/createform");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+app.use("/api/users", userApiRoutes);
+app.use("/api/widgets", widgetApiRoutes);
+app.use("/users", usersRoutes);
+app.use("/create", createPost);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get('/', (req, res) => {
-  res.render('homepage');
+app.get("/", (req, res) => {
+  res.render("homepage");
 });
 
 // My Listings
-app.get('/mylistings', (req, res) => {
-  res.render('mylistings');
-});
-
-// Create Form
-app.get('/create', (req, res) => {
-  res.render('createform');
-});
-
-// Post Create Form
-app.post('/create', (req, res) => {
-  // Code to process the form data goes here
-  // After processing the form, redirect back to the homepage with the new data
-  res.redirect('/');
+app.get("/mylistings", (req, res) => {
+  res.render("mylistings");
 });
 
 // Favourites
-app.get('/favourites', (req, res) => {
-  res.render('favourites');
+app.get("/favourites", (req, res) => {
+  res.render("favourites");
 });
 
 app.listen(PORT, () => {
