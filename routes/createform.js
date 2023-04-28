dad2c942ebfe23621c4037afc1d2237f2a32d0ea
Tony Chow <tchow9@gmail.com>
2023-04-28 16:44:50 -0700

    Updating label

--- routes/createform.js
+++ routes/createform.js
@@ -1,25 +1,26 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
+ * All routes to create a new post are defined here
+ * Since this file is loaded in server.js into /create,
+ *   these routes are mounted onto /create
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require("express");
 const router = express.Router();
+const db = require("../db/connection");
 
 router.get("/", (req, res) => {
   res.render("createform");
 });
 
-router.get("/:id", (req, res) => {
-  if (req.session.user_id === "1") {
-    console.log("logged in users");
-  }
-  console.log(req.session);
-  console.log(req.params["id"]);
-  res.render("createform");
-});
+// router.get("/:id", (req, res) => {
+//   if (req.session.user_id === "1") {
+//     console.log("logged in users");
+//   }
+//   console.log(req.session);
+//   console.log(req.params["id"]);
+//   res.render("createform");
+// });
 
 router.post("/", (req, res) => {
   console.log(req.session.user_id);
