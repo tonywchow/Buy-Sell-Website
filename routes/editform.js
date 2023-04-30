1156d5f635d93e4fe09e28d801f9cd5b491a5035
Tony Chow <tchow9@gmail.com>
2023-04-30 09:39:45 -0700

    Add sass to create and edit pages

--- routes/editform.js
+++ routes/editform.js
@@ -12,7 +12,7 @@ const databaseQueries = require("../db/queries/databaseQueries");
 //This GET HTTP request is to get product information from productID once the edit button is clicked in /mylistings route. This will auto populate the forms in the EJS file
 router.get("/", (req, res) => {
   databaseQueries
-    .getProductWithId(2) //currently the productID is hardcoded in the method for testing. Medhanie to integrate
+    .getProductWithId(1) //currently the productID is hardcoded in the method for testing. Medhanie to integrate
     .then((product) => {
       console.log(product.price);
       res.render("editform", { product });
@@ -24,8 +24,8 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const changesToProduct = req.body;
   databaseQueries
-    .editProduct(2, changesToProduct) //currently the productID is hardcoded in the method for testing. Medhanie to integrate. editProduct function takes in two parameters, the productID and the information from the forms through req.body
-    .then(res.redirect("/mylistings"))
+    .editProduct(1, changesToProduct) //currently the productID is hardcoded in the method for testing. Medhanie to integrate. editProduct function takes in two parameters, the productID and the information from the forms through req.body
+    .then(res.redirect("/edit"))
     .catch((error) => res.send(error));
 });
 
