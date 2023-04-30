cdad2957f5aecd4d6e9d9347c5a0d126a8c51a98
Tony Chow <tchow9@gmail.com>
2023-04-30 07:49:35 -0700

    Added comments to editform and cleaned up databaseQueries

--- routes/editform.js
+++ routes/editform.js
@@ -1,7 +1,7 @@
 /*
- * All routes to create a new post are defined here
- * Since this file is loaded in server.js into /create,
- *   these routes are mounted onto /create
+ * All routes to edit a new post are defined here
+ * Since this file is loaded in server.js into /edit,
+ *   these routes are mounted onto /edit
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
@@ -9,9 +9,10 @@ const express = require("express");
 const router = express.Router();
 const databaseQueries = require("../db/queries/databaseQueries");
 
+//This GET HTTP request is to get product information from productID once the edit button is clicked in /mylistings route. This will auto populate the forms in the EJS file
 router.get("/", (req, res) => {
   databaseQueries
-    .getProductWithId(2)
+    .getProductWithId(2) //currently the productID is hardcoded in the method for testing. Medhanie to integrate
     .then((product) => {
       console.log(product.price);
       res.render("editform", { product });
@@ -19,10 +20,11 @@ router.get("/", (req, res) => {
     .catch((error) => res.send(error));
 });
 
+//This POST HTTP request is to POST the edited product information once the submit button is clicked
 router.post("/", (req, res) => {
   const changesToProduct = req.body;
   databaseQueries
-    .editProduct(2, changesToProduct)
+    .editProduct(2, changesToProduct) //currently the productID is hardcoded in the method for testing. Medhanie to integrate. editProduct function takes in two parameters, the productID and the information from the forms through req.body
     .then(res.redirect("/mylistings"))
     .catch((error) => res.send(error));
 });
