c3506d144b4e276a64f9d3f652f8afaedf9600e9
Tony Chow <tchow9@gmail.com>
2023-05-01 06:30:08 -0700

    Modularize email query

--- db/queries/get-email-with-id.js
+++ db/queries/get-email-with-id.js
@@ -1,3 +1,28 @@
 const db = require("../connection");
 
-//Get 
+/*
+Get user email from user id
+*/
+const getEmailWithId = function (id) {
+  const queryString = `
+  SELECT * FROM users
+  WHERE id = $1
+  `;
+  console.log("database query test", id);
+  const values = [id];
+  return db
+    .query(queryString, values)
+    .then((result) => {
+      if (result.rows) {
+        // console.log("test", result.rows);
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((error) => console.log(error));
+};
+
+module.exports = {
+  getEmailWithId,
+};
