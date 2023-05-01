b54ccc08519226d01707b2f8a7d35761ab4fdf54
Tony Chow <tchow9@gmail.com>
2023-05-01 06:22:56 -0700

    Add modularize queries to routes

--- routes/editform.js
+++ routes/editform.js
@@ -7,11 +7,12 @@
 
 const express = require("express");
 const router = express.Router();
-const databaseQueries = require("../db/queries/databaseQueries");
+const productQuery = require("../db/queries/get-product-with-id");
+const editQuery = require("../db/queries/edit-product");
 
 //This GET HTTP request is to get product information from productID once the edit button is clicked in /mylistings route. This will auto populate the forms in the EJS file
 router.get("/", (req, res) => {
-  databaseQueries
+  productQuery
     .getProductWithId(1) //currently the productID is hardcoded in the method for testing. Medhanie to integrate
     .then((product) => {
       console.log(product.price);
@@ -23,7 +24,7 @@ router.get("/", (req, res) => {
 //This POST HTTP request is to POST the edited product information once the submit button is clicked
 router.post("/", (req, res) => {
   const changesToProduct = req.body;
-  databaseQueries
+  editQuery
     .editProduct(1, changesToProduct) //currently the productID is hardcoded in the method for testing. Medhanie to integrate. editProduct function takes in two parameters, the productID and the information from the forms through req.body
     .then(res.redirect("/edit"))
     .catch((error) => res.send(error));
