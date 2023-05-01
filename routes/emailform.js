f916296b9f3d298ce84c46ddf7fb7589cb823bb6
Tony Chow <tchow9@gmail.com>
2023-04-30 17:05:10 -0700

    Add email api using SendGrid

--- routes/emailform.js
+++ routes/emailform.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const databaseQueries = require("../db/queries/databaseQueries");
+const sgMail = require("@sendgrid/mail");
 
 router.get("/", (req, res) => {
   console.log("the users id", req.session.user_id);
@@ -24,6 +25,18 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   console.log(req.body);
+  const email = req.body;
+  const message = {
+    to: "lighthouselabtest1@gmail.com",
+    from: "lighthouselabtest@gmail.com",
+    subject: email.subject,
+    text: email.message,
+  };
+  console.log(message);
+  sgMail
+    .send(message)
+    .then((result) => console.log("Email sent..."))
+    .catch((error) => console.log(error.message));
 });
 
 module.exports = router;
