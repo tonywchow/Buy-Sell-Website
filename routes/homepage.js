b6dde581972918c85babc1b98e220e28d81e4535
Tony Chow <tchow9@gmail.com>
2023-05-01 19:58:00 -0700

    Integrated email link to email form route

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const router  = express.Router();
-const db = require('../db/connection');
+const express = require("express");
+const router = express.Router();
+const db = require("../db/connection");
 
-router.get('/', (req, res) => {
-  const query = `SELECT * FROM products`;
+router.get("/", (req, res) => {
+  const query = `
+  SELECT products.*, users.id as user_id, name, email
+  FROM products
+  JOIN users on users.id = user_id
+  `;
   db.query(query)
-    .then(data => {
+    .then((data) => {
       const products = data.rows;
-      res.render('homepage', { products });
+      res.render("homepage", { products });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).send('Error retrieving products from database');
+      res.status(500).send("Error retrieving products from database");
     });
 });
 
 module.exports = router;
-
