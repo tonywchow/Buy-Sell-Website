79e5819073662ba37ca338032e999199aa3660fb
Medhanie Alene <alenemedhanie@gmail.com>
2023-05-02 23:06:15 -0400

    Update mylistings.js with merged file

--- routes/mylistings.js
+++ routes/mylistings.js
@@ -3,24 +3,6 @@ const router = express.Router();
 const db = require("../db/connection");
 const { getProductByUserId } = require("../db/queries/get-product-by-userID");
 
-//Mark item as sold
-router.post("/:product_id/sold", (req, res) => {
-  const product_id = req.params.product_id;
-  const qs = `UPDATE products
-              SET availability = false, title= 'SOLD!'
-              WHERE products.id = ${product_id};`;
-
-//   db.query(qs)
-//     .then((result) => {
-//       console.log("result: ", result);
-//       res.redirect("/mylistings");
-//     })
-//     .catch((err) => {
-//       console.log("error: ", err);
-//       res.send("Product does not exist!");
-//     });
-// });
-
 // Route to show products for a specific user
 router.get("/", (req, res) => {
   const userId = req.session.user_id;
