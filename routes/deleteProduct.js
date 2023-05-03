ff923a775c79a312e2d0ab10e91c06ff58d6abbf
Medhanie Alene <alenemedhanie@gmail.com>
2023-05-02 23:24:26 -0400

    add query function delete-product.js

--- routes/deleteProduct.js
+++ routes/deleteProduct.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db/connection");
+const deleteProduct = require("../db/queries/delete-product");
 
 //Delete product
 router.post("/:product_id/delete", (req, res) => {
   const product_id = req.params.product_id;
-  console.log("#1 product_id: ", product_id);
-  console.log("#2 result: ", req.body);
-  const qs = `DELETE FROM products WHERE products.id=${product_id}`;
 
-  db.query(qs)
+  deleteProduct.deleteProductById(product_id)
     .then((result) => {
       console.log("result is: ", result);
-      // res.send('Product deleted!');
       res.redirect("/mylistings");
     })
     .catch((err) => {
