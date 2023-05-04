5249655a544aaae33ca99f604c70510e84432959
Kevin Cogen <k.cogen@gmail.com>
2023-05-03 22:53:25 -0400

    dynamic loading

--- routes/homepage.js
+++ routes/homepage.js
@@ -5,10 +5,10 @@ const db = require("../db/connection");
 router.get("/", (req, res) => {
   const userId = req.session.user_id;
   const query = `
-    SELECT products.*, users.id as user_id, name, email, favourites.user_id as fav_user_id
-    FROM products
-    JOIN users ON users.id = products.user_id
-    LEFT JOIN favourites ON favourites.product_id = products.id AND favourites.user_id = $1;
+  SELECT products.*, users.id AS user_id, name, email, COALESCE(favourites.id, 0) AS favourite_id
+  FROM products
+  JOIN users ON users.id = products.user_id
+  LEFT JOIN favourites ON favourites.product_id = products.id AND favourites.user_id = $1;
   `;
   db.query(query, [userId])
     .then((data) => {
@@ -21,5 +21,5 @@ router.get("/", (req, res) => {
     });
 });
 
-module.exports = router;
 
+module.exports = router;
