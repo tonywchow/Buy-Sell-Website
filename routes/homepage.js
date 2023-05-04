122bded1ab4d8148b90167e578cdfc821b719476
Kevin Cogen <k.cogen@gmail.com>
2023-05-03 22:41:28 -0400

    favourite button edits

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const db = require("../db/connection");
 
 router.get("/", (req, res) => {
+  const userId = req.session.user_id;
   const query = `
-  SELECT products.*, users.id as user_id, name, email
-  FROM products
-  JOIN users on users.id = user_id
+    SELECT products.*, users.id as user_id, name, email, favourites.user_id as fav_user_id
+    FROM products
+    JOIN users ON users.id = products.user_id
+    LEFT JOIN favourites ON favourites.product_id = products.id AND favourites.user_id = $1;
   `;
-  db.query(query)
+  db.query(query, [userId])
     .then((data) => {
       const products = data.rows;
       res.render("homepage", { products });
@@ -20,3 +22,4 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
+
