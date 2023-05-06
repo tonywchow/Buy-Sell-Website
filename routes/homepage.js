576a25339c9671ba5133cf191d5436bf384b8e16
Tony Chow <tchow9@gmail.com>
2023-05-06 09:44:18 -0700

    Add feature listing

--- routes/homepage.js
+++ routes/homepage.js
@@ -4,14 +4,24 @@ const db = require("../db/connection");
 
 router.get("/", (req, res) => {
   const queryParams = []
+  let query = '';
   const userId = req.session.user_id;
-  const query = `
-  SELECT products.*, users.id AS user_id, name, email, COALESCE(favourites.id, 0) AS favourite_id
-  FROM products
-  JOIN users ON users.id = products.user_id
-  LEFT JOIN favourites ON favourites.product_id = products.id AND favourites.user_id = $1
-  ORDER BY timestamp DESC;
-  `;
+  if (!userId) {
+     query = `
+     SELECT products.*, users.id AS user_id, name, email, COALESCE(favourites.id, 0) AS favourite_id
+    FROM products
+    JOIN users ON users.id = products.user_id
+    LEFT JOIN favourites ON favourites.product_id = products.id AND favourites.user_id = $1
+    ORDER BY timestamp ASC;`
+  } else {
+    query = `
+    SELECT products.*, users.id AS user_id, name, email, COALESCE(favourites.id, 0) AS favourite_id
+    FROM products
+    JOIN users ON users.id = products.user_id
+    LEFT JOIN favourites ON favourites.product_id = products.id AND favourites.user_id = $1
+    ORDER BY timestamp DESC;
+    `;
+  }
   db.query(query, [userId])
     .then((data) => {
       const products = data.rows;
